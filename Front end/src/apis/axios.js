ab24e7d1f970045e4217460b429a03c75c1e375c
MohamedMahmoud01110 <mohamed.mahmoud14532@gmail.com>
2026-05-27 20:35:41 +0300

    update front url

--- Front end/src/apis/axios.js
+++ Front end/src/apis/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
-  // baseURL: "https://hiremind-production.up.railway.app/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://hiremind-production.up.railway.app/api",
 });
 
 export const AnalyzeCvAPI = axios.create({
